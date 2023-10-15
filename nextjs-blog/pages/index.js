5b1086fa7185965e0340b041d55f8bd316181002
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-10-14 18:40:43 -0700

    public/images

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className={styles.title}>
+        Read <Link href="/posts/first-post">this page!</Link>
+      </h1>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
