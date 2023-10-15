761ef2beb228b4fa61f451ee8a9dc63b210e1d00
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-10-14 20:21:20 -0700

    initial commit-1

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <Link href="/posts/first-post">Check this page!</Link>
       </section>
     </Layout>
   );
