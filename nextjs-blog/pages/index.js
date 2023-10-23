9c6bb0efba6b2e5e08e7dc9983caff7d956b153f
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-10-22 21:55:20 -0700

    fixing webpack-internal error

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,20 +2,47 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
+
+  // Fetch popular movies data here
+  const popularMovies = await fetchPopularMovies();
+
   return {
     props: {
-      allPostsData
+      allPostsData,
+      popularMovies,
     },
   };
 }
-console.log()
-export default function Home({ allPostsData }, {allMovies}) {
-  console.log('allMovies');
+
+async function fetchPopularMovies() {
+  const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZmUwODc0OGM2MDk2NTkxNTM2ODZkYWY0YWNkYWZlZSIsInN1YiI6IjY1MzAwZDUxMGI3NGU5MDEzODdmY2MwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ITePXKabQvQXZyZLELNVoPSedLn1m8cxLnxEGGrtalI'
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching popular movies:', error);
+    return null;
+  }
+}
+
+export default function Home({ allPostsData, popularMovies }) {
+  console.log('popularMovies', popularMovies)
   return (
     <Layout home>
       <Head>
@@ -43,6 +70,8 @@ export default function Home({ allPostsData }, {allMovies}) {
             </li>
           ))}
         </ul>
+      </section>
+      <section>
       </section>
     </Layout>
   );
