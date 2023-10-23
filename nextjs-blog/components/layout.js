7f8c9c631bb9fdea5fb84d9b53fd99d6b99a10a8
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-10-22 22:38:19 -0700

    home page and now playing

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Yuliia Pchelintseva';
@@ -26,37 +24,15 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
+        <h1 className={styles.siteTitle}>
+          {home ? (
+            name
+          ) : (
             <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
+              {name}
             </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
+          )}
+        </h1>
       </header>
       <main>{children}</main>
       {!home && (
---
+++ nextjs-blog/pages/now-playing.js
@@ -0,0 +1,68 @@
+import Layout from '../components/layout';
+import { useState, useEffect } from 'react';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+export default function NowPlaying() {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1); // Initialize with page 1
+
+  const loadNextPage = () => {
+    // Increment the page number and update the state
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`;
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZmUwODc0OGM2MDk2NTkxNTM2ODZkYWY0YWNkYWZlZSIsInN1YiI6IjY1MzAwZDUxMGI3NGU5MDEzODdmY2MwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ITePXKabQvQXZyZLELNVoPSedLn1m8cxLnxEGGrtalI',
+          },
+        };
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setMovies((prevMovies) => [...prevMovies, ...data.results]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchMovies();
+  }, [page]); // Fetch when the page state changes
+
+  return (
+    <Layout>
+      <Link style={{ textDecoration: 'underline', cursor: 'pointer' }} href="/">Back to Top Rated Movies</Link>
+      <div>
+        <h1>Now Playing Movies:</h1>
+        <ul className={utilStyles.moviesList}>
+          {movies.map((movie) => (
+            <li key={movie.id} className={utilStyles.movieItem}>
+              <img
+                src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <div className={utilStyles.movieInfo}>
+                <p className={utilStyles.movieTitle}>Title: {movie.title}</p>
+                <p className={utilStyles.movieOverview}>{movie.overview}</p>
+                <p className={utilStyles.movieRating}>Rating: {movie.vote_average}</p>
+                {/* Add more movie details as needed */}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button className={utilStyles.loadMoreButton} onClick={loadNextPage}>
+          Load More
+        </button>
+      </div>
+    </Layout>
+  );
+}
